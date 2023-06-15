9e2686ff4176cc3e83f728827c96a7f1a80c9409
Matt Vail <mattvailco@gmail.com>
2023-06-15 15:54:13 -0600

    solution for 7kyu triangular treasure

---
+++ 06.15.23-7kyu-triangular-treasure/solution.js
@@ -0,0 +1,6 @@
+//Solution for 7 kyu Triangular Treasure 
+//Link to Kata: https://www.codewars.com/kata/525e5a1cb735154b320002c8
+
+function triangular( n ) {
+    return n <= 0 ? 0 : n * (n + 1) / 2;
+  }
